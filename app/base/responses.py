525281c08b96be1bc8e18f814b33c1c6026296a6
MohamedRaffik <raffikmohamed7@gmail.com>
2025-06-18 13:14:36 -0400

    refactor(base): remove base folder

--- app/base/responses.py
+++
@@ -1,9 +0,0 @@
-from pydantic import BaseModel, ConfigDict, alias_generators
-
-
-class ResponseBaseModel(BaseModel):
-    model_config = ConfigDict(
-        alias_generator=alias_generators.to_camel,
-        populate_by_name=True,
-        from_attributes=True,
-    )
