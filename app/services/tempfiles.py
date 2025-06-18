b1bf3c912212d9fb0d2792141c241e17a0575fef
MohamedRaffik <raffikmohamed7@gmail.com>
2025-06-18 13:11:55 -0400

    refactor(tempfiles): rewrite tempfiles

--- app/services/tempfiles.py
+++ app/services/tempfiles.py
@@ -1,24 +1,29 @@
 import asyncio
-import os
+import shutil
+from pathlib import Path
+
+import aiofiles
+import aiofiles.os
 
 TEMP_DIRECTORY = "temp"
 
 
-async def _create_temp_directory():
+async def cleanup_temp_directory():
+    await asyncio.to_thread(shutil.rmtree, TEMP_DIRECTORY)
+
+
+async def create_temp_directory():
     try:
-        await asyncio.to_thread(os.mkdir, TEMP_DIRECTORY)
+        await aiofiles.os.mkdir(TEMP_DIRECTORY)
     except FileExistsError:
         pass
 
 
-async def create_new_directory(directory: str, raise_on_exists=True):
-    directory_path = os.path.join(TEMP_DIRECTORY, os.path.normpath(directory))
-    await _create_temp_directory()
-    if not raise_on_exists:
-        try:
-            await asyncio.to_thread(os.mkdir, directory_path)
-        except FileExistsError:
-            pass
-    else:
-        await asyncio.to_thread(os.mkdir, directory_path)
+async def create_new_directory(directory: str):
+    directory_path = Path(TEMP_DIRECTORY).resolve().joinpath(Path(directory))
+    await create_temp_directory()
+    try:
+        await aiofiles.os.mkdir(directory_path)
+    except FileExistsError:
+        pass
     return directory_path
