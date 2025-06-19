63d1dd67fc72dad2dd271ddd86c2e123d0923c43
MohamedRaffik <raffikmohamed7@gmail.com>
2025-06-19 02:05:27 -0400

    refactor(dependencies): rework dependencies to allow reading from cookies as well

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,7 +2,7 @@ from contextlib import asynccontextmanager
 from typing import Annotated
 
 import jwt
-from fastapi import Depends, HTTPException, status
+from fastapi import Cookie, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -37,19 +37,34 @@ async def get_user_from_token(db_session: DatabaseSession, token: Token):
 TokenUser = Annotated[User | None, Depends(get_user_from_token)]
 
 
-async def get_authenticated_user_from_token(user: TokenUser):
-    if not user:
+async def get_user_from_cookie(
+    db_session: DatabaseSession, token: Annotated[str | None, Cookie()]
+):
+    if token:
+        payload = jwt.decode(token, key=settings.secret_key, algorithms=["HS256"])
+        if username := payload.get("sub"):
+            query = select(User).where(User.email == username)
+            user = await db_session.scalar(query)
+            return user
+    return None
+
+
+CookieUser = Annotated[User | None, Depends(get_user_from_cookie)]
+
+
+async def get_authenticated_user(token_user: TokenUser, cookie_user: CookieUser):
+    if not (user := token_user or cookie_user):
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
     return user
 
 
-AuthTokenUser = Annotated[User, Depends(get_authenticated_user_from_token)]
+AuthUser = Annotated[User, Depends(get_authenticated_user)]
 
 
-async def get_admin_user_from_token(user: AuthTokenUser):
+async def get_admin_user(user: AuthUser):
     if not user.admin:
         raise HTTPException(status_code=status.HTTP_403_FORBIDDEN)
     return user
 
 
-AdminTokenUser = Annotated[User, Depends(get_admin_user_from_token)]
+AdminUser = Annotated[User, Depends(get_admin_user)]
