eca50a63981c30b1212d185756ddbfbc0381eac7
MohamedRaffik <raffikmohamed7@gmail.com>
2025-06-20 15:19:57 -0400

    refactor(music): add new music router to application

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,12 @@ from fastapi import APIRouter, FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 
 from app.routes.authentication import router as auth_router
+from app.routes.music import router as music_router
 from app.settings import ENV, settings
 
 api_router = APIRouter(prefix="/api")
 api_router.include_router(auth_router)
+api_router.include_router(music_router)
 
 
 app = FastAPI(title="dripdrop", docs_url="/api/docs", openapi_url="/api/openapi.json")
