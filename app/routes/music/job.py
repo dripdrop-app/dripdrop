79d7c5843577ff5d7213723bd66c7960134c9b10
MohamedRaffik <raffikmohamed7@gmail.com>
2025-06-20 16:02:24 -0400

    refactor(music): add job endpoint for music router, remove extra files

--- app/routes/music/job.py
+++ app/routes/music/job.py
@@ -1,147 +1,99 @@
-import asyncio
 import re
-import uuid
-from typing import Optional
-from urllib.parse import quote
+from typing import Annotated
 
-from fastapi import (
-    APIRouter,
-    BackgroundTasks,
-    Depends,
-    File,
-    Form,
-    HTTPException,
-    Path,
-    Response,
-    UploadFile,
-    status,
-)
-from fastapi.responses import StreamingResponse
-from pydantic import HttpUrl
-from rq.job import Retry
-from sqlalchemy import select
+from fastapi import APIRouter, BackgroundTasks, Depends, Form, HTTPException, status
 
-from app.authentication.dependencies import (
-    AuthenticatedUser,
-    get_authenticated_user,
-)
-from app.base.dependencies import DatabaseSession
-from app.music import tasks, utils
-from app.music.models import MusicJob
-from app.music.responses import (
-    ErrorMessages,
-    MusicJobResponse,
-    MusicJobUpdateResponse,
-)
-from app.services import http_client, rq_client
-from app.services.websocket_channel import RedisChannels, WebsocketChannel
-from app.utils import get_current_time
+from app.db import MusicJob
+from app.dependencies import AuthUser, DatabaseSession, get_authenticated_user
+from app.models.music import CreateMusicJob
+from app.tasks.music import run_music_job
 
-api = APIRouter(
+router = APIRouter(
     prefix="/job",
     tags=["Music Job"],
     dependencies=[Depends(get_authenticated_user)],
 )
 
 
-@api.post("/create", status_code=status.HTTP_201_CREATED)
+@router.post("/create", status_code=status.HTTP_201_CREATED)
 async def create_job(
-    user: AuthenticatedUser,
+    user: AuthUser,
     session: DatabaseSession,
     background_tasks: BackgroundTasks,
-    file: Optional[UploadFile] = File(None),
-    video_url: Optional[HttpUrl] = Form(None),
-    artwork_url: Optional[str] = Form(None),
-    title: str = Form(...),
-    artist: str = Form(...),
-    album: str = Form(...),
-    grouping: Optional[str] = Form(None),
+    form: Annotated[CreateMusicJob, Form()],
 ):
-    if file and video_url:
+    if form.file and form.video_url:
         raise HTTPException(
-            detail=ErrorMessages.CREATE_JOB_BOTH_DEFINED,
+            detail="'file' and 'video_url' cannot both be defined.",
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         )
-    elif file is None and video_url is None:
+    elif form.file is None and form.video_url is None:
         raise HTTPException(
-            detail=ErrorMessages.CREATE_JOB_NOT_DEFINED,
+            detail="'file' or 'video_url' must be defined.",
             status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
         )
-    if file:
-        if not re.match("audio/", file.content_type):
+    if form.file:
+        if not re.match("^audio/", form.file.content_type):
             raise HTTPException(
-                detail=ErrorMessages.FILE_INCORRECT_FORMAT,
+                detail="File is incorrect format.",
                 status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
             )
-    job_id = str(uuid.uuid4())
     music_job = MusicJob(
-        id=job_id,
         user_email=user.email,
-        video_url=video_url.unicode_string() if video_url else None,
-        title=title,
-        artist=artist,
-        album=album,
-        grouping=grouping,
-        completed=False,
-        failed=False,
+        video_url=form.video_url.unicode_string() if form.video_url else None,
+        title=form.title,
+        artist=form.artist,
+        album=form.album,
+        grouping=form.grouping,
     )
     session.add(music_job)
     await session.commit()
-    await WebsocketChannel(channel=RedisChannels.MUSIC_JOB_UPDATE).publish(
-        message=MusicJobUpdateResponse(id=job_id, status="STARTED")
-    )
-    await utils.handle_files(job_id=job_id, file=file, artwork_url=artwork_url)
-    background_tasks.add_task(
-        rq_client.high.enqueue,
-        tasks.run_music_job,
-        music_job_id=job_id,
-        retry=Retry(2, [10, 30]),
-    )
-    return MusicJobResponse.model_validate(music_job)
+    background_tasks.add_task(music_job.upload_files, form.file, form.artwork_url)
+    background_tasks.add_task(run_music_job.delay, music_job_id=str(music_job.id))
 
 
-@api.delete(
-    "/{job_id}/delete",
-    responses={status.HTTP_404_NOT_FOUND: {"description": ErrorMessages.JOB_NOT_FOUND}},
-)
-async def delete_job(
-    user: AuthenticatedUser, session: DatabaseSession, job_id: str = Path(...)
-):
-    query = select(MusicJob).where(
-        MusicJob.id == job_id, MusicJob.user_email == user.email
-    )
-    music_job = await session.scalar(query)
-    if not music_job:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=ErrorMessages.JOB_NOT_FOUND
-        )
-    await asyncio.to_thread(rq_client.stop_job, job_id=job_id)
-    await music_job.cleanup()
-    music_job.deleted_at = get_current_time()
-    await session.commit()
-    return Response(None)
+# @api.delete(
+#     "/{job_id}/delete",
+#     responses={status.HTTP_404_NOT_FOUND: {"description": ErrorMessages.JOB_NOT_FOUND}},
+# )
+# async def delete_job(
+#     user: AuthenticatedUser, session: DatabaseSession, job_id: str = Path(...)
+# ):
+#     query = select(MusicJob).where(
+#         MusicJob.id == job_id, MusicJob.user_email == user.email
+#     )
+#     music_job = await session.scalar(query)
+#     if not music_job:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail=ErrorMessages.JOB_NOT_FOUND
+#         )
+#     await asyncio.to_thread(rq_client.stop_job, job_id=job_id)
+#     await music_job.cleanup()
+#     music_job.deleted_at = get_current_time()
+#     await session.commit()
+#     return Response(None)
 
 
-@api.get("/{job_id}/download", responses={status.HTTP_404_NOT_FOUND: {}})
-async def download_job(session: DatabaseSession, job_id: str = Path(...)):
-    query = select(MusicJob).where(MusicJob.id == job_id)
-    music_job = await session.scalar(query)
-    if not music_job:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail=ErrorMessages.JOB_NOT_FOUND
-        )
-    if not music_job.download_url:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND,
-            detail=ErrorMessages.DOWNLOAD_NOT_FOUND,
-        )
-    filename = music_job.download_filename.split("/")[-1]
-    async with http_client.create_client() as client:
-        response = await client.get(music_job.download_url)
-        return StreamingResponse(
-            content=response.aiter_bytes(chunk_size=500),
-            media_type=response.headers.get("content-type"),
-            headers={
-                "Content-Disposition": f"attachment; filename*=UTF-8''{quote(filename)}"
-            },
-        )
+# @api.get("/{job_id}/download", responses={status.HTTP_404_NOT_FOUND: {}})
+# async def download_job(session: DatabaseSession, job_id: str = Path(...)):
+#     query = select(MusicJob).where(MusicJob.id == job_id)
+#     music_job = await session.scalar(query)
+#     if not music_job:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND, detail=ErrorMessages.JOB_NOT_FOUND
+#         )
+#     if not music_job.download_url:
+#         raise HTTPException(
+#             status_code=status.HTTP_404_NOT_FOUND,
+#             detail=ErrorMessages.DOWNLOAD_NOT_FOUND,
+#         )
+#     filename = music_job.download_filename.split("/")[-1]
+#     async with http_client.create_client() as client:
+#         response = await client.get(music_job.download_url)
+#         return StreamingResponse(
+#             content=response.aiter_bytes(chunk_size=500),
+#             media_type=response.headers.get("content-type"),
+#             headers={
+#                 "Content-Disposition": f"attachment; filename*=UTF-8''{quote(filename)}"
+#             },
+#         )
