22171af6f5ec52d90eba047ab1b6ab01f4ef1810
MohamedRaffik <raffikmohamed7@gmail.com>
2025-06-20 11:13:40 -0400

    refactor(jwt): add better typing for jwt methods, allow jwt to auto check expiration

--- app/services/jwt.py
+++ app/services/jwt.py
@@ -2,19 +2,25 @@ import asyncio
 from datetime import datetime, timedelta, timezone
 
 import jwt
+from pydantic import BaseModel, EmailStr
 
 from app.settings import settings
 
 ALGORITHM = "HS256"
 
 
+class JWTPayload(BaseModel):
+    sub: EmailStr
+    exp: float
+
+
 async def create_jwt(email: str):
     return await asyncio.to_thread(
         jwt.encode,
-        payload={
-            "email": email,
-            "exp": datetime.now(timezone.utc) + timedelta(days=14),
-        },
+        payload=JWTPayload(
+            sub=email,
+            exp=(datetime.now(timezone.utc) + timedelta(days=14)).timestamp(),
+        ).model_dump(),
         key=settings.secret_key,
         algorithm=ALGORITHM,
     )
@@ -23,12 +29,12 @@ async def create_jwt(email: str):
 async def decode_jwt(token: str):
     try:
         payload = await asyncio.to_thread(
-            jwt.decode, token, key=settings.secret_key, algorithms=[ALGORITHM]
+            jwt.decode,
+            token,
+            key=settings.secret_key,
+            algorithms=[ALGORITHM],
+            options={"verify_exp": True},
         )
-        if (expires := payload.get("exp")) and expires < datetime.now(
-            timezone.utc
-        ).timestamp():
-            return payload
-        return None
+        return JWTPayload(**payload)
     except jwt.PyJWTError:
         return None
