511448a336fcaefdc2392bec88b5fdecd85cf6e6
MohamedRaffik <raffikmohamed7@gmail.com>
2025-06-25 23:45:45 +0000

    test(test_verify): increase test coverage for verify endpoint

--- tests/routes/authentication/test_verify.py
+++ tests/routes/authentication/test_verify.py
@@ -1,23 +1,37 @@
+import pytest
 from fastapi import status
+from fastapi.exceptions import HTTPException
 
 from app.db import User
+from app.routes.authentication import verify_email
 
 URL = "/api/auth/verify"
 
 
-async def test_verify_with_invalid_code(client, faker, create_user):
+@pytest.mark.parametrize("use_function", [True, False])
+async def test_verify_with_invalid_code(
+    client, faker, create_user, use_function, db_session, redis
+):
     """
     Test verifying an account with an invalid code. The endpoint should return
     a 400 error with the appropriate message.
     """
 
     await create_user(verified=False)
-    response = await client.get(URL, params={"token": faker.uuid4()})
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json() == {"detail": "Token is not valid."}
+
+    if use_function:
+        with pytest.raises(HTTPException):
+            await verify_email(db_session, redis, faker.uuid4())
+    else:
+        response = await client.get(URL, params={"token": faker.uuid4()})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json() == {"detail": "Token is not valid."}
 
 
-async def test_verify_with_nonexistent_email(client, faker, redis):
+@pytest.mark.parametrize("use_function", [True, False])
+async def test_verify_with_nonexistent_email(
+    client, faker, redis, use_function, db_session
+):
     """
     Test verifying with a code for an email that no longer exists. The endpoint should
     return a 400 error with the appropriate message.
@@ -25,12 +39,18 @@ async def test_verify_with_nonexistent_email(client, faker, redis):
 
     token = faker.uuid4()
     await redis.set(f"verify:{token}", faker.email())
-    response = await client.get(URL, params={"token": token})
-    assert response.status_code == status.HTTP_400_BAD_REQUEST
-    assert response.json() == {"detail": "Account does not exist."}
+
+    if use_function:
+        with pytest.raises(HTTPException):
+            await verify_email(db_session, redis, token)
+    else:
+        response = await client.get(URL, params={"token": token})
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.json() == {"detail": "Account does not exist."}
 
 
-async def test_verify(client, create_user, faker, db_session, redis):
+@pytest.mark.parametrize("use_function", [True, False])
+async def test_verify(client, create_user, faker, db_session, redis, use_function):
     """
     Test verifying an account with a valid token. The endpoint should return a 204
     response.
@@ -40,8 +60,12 @@ async def test_verify(client, create_user, faker, db_session, redis):
     token = faker.uuid4()
     await redis.set(f"verify:{token}", user.email)
 
-    response = await client.get(URL, params={"token": token})
-    assert response.status_code == status.HTTP_204_NO_CONTENT
+    if use_function:
+        await verify_email(db_session, redis, token)
+        assert await redis.get(f"verify:{token}") is None
+    else:
+        response = await client.get(URL, params={"token": token})
+        assert response.status_code == status.HTTP_204_NO_CONTENT
 
     await db_session.refresh(user)
     assert user.verified is True
