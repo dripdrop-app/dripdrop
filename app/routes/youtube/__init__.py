ecd03a42243f575f61e4f76f54b2fd665ae16e7a
MohamedRaffik <raffikmohamed7@gmail.com>
2025-07-03 00:56:11 +0000

    fix(youtube): fix mounting youtube router in application

--- app/routes/youtube/__init__.py
+++ app/routes/youtube/__init__.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, Depends, status
 
 from app.dependencies import get_authenticated_user
 
-api = APIRouter(
+router = APIRouter(
     prefix="/youtube",
     tags=["YouTube"],
     dependencies=[Depends(get_authenticated_user)],
