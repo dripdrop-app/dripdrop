e55ed1a2798cc01a0f2a1f41b6ebf977c247955e
MohamedRaffik <raffikmohamed7@gmail.com>
2025-07-27 20:09:34 -0400

    test(youtube): add tests for adding a youtube video like

---
+++ tests/routes/youtube/test_add_video_like.py
@@ -0,0 +1,88 @@
+import pytest
+from fastapi import HTTPException, status
+from sqlalchemy import select
+
+from app.db import User, YoutubeVideo, YoutubeVideoLike
+from app.routes.youtube.videos import add_youtube_video_like
+
+URL = "/api/youtube/videos/{video_id}/like"
+
+
+async def test_add_video_like_when_not_logged_in(client, faker):
+    """
+    Test adding a youtube video like when not logged in. The endpoint
+    should return a 401 status.
+    """
+
+    response = await client.put(URL.format(video_id=faker.uuid4()))
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
+
+
+@pytest.mark.parametrize("use_function", [True, False])
+async def test_add_video_like_for_non_existent_video(
+    client, faker, create_and_login_user, db_session, use_function
+):
+    """
+    Test adding a youtube video like for a video that does not exist. The
+    endpoint should return a 404 status.
+    """
+
+    user: User = await create_and_login_user()
+
+    if use_function:
+        with pytest.raises(HTTPException):
+            await add_youtube_video_like(user, db_session, video_id=faker.uuid4())
+    else:
+        response = await client.put(URL.format(video_id=faker.uuid4()))
+        assert response.status_code == status.HTTP_404_NOT_FOUND
+        assert response.json() == {"detail": "Youtube video not found."}
+
+
+@pytest.mark.parametrize("use_function", [True, False])
+async def test_add_video_like(
+    client, create_and_login_user, create_youtube_video, db_session, use_function
+):
+    """
+    Test adding a youtube video like for a video. The endpoint should return
+    a 200 status.
+    """
+
+    user: User = await create_and_login_user()
+    video: YoutubeVideo = await create_youtube_video()
+
+    if use_function:
+        await add_youtube_video_like(user, db_session, video_id=video.id)
+    else:
+        response = await client.put(URL.format(video_id=video.id))
+        assert response.status_code == status.HTTP_200_OK
+
+    like = await db_session.scalar(
+        select(YoutubeVideoLike).where(
+            YoutubeVideoLike.email == user.email,
+            YoutubeVideoLike.video_id == video.id,
+        )
+    )
+    assert like
+
+
+async def test_add_video_like_with_existing_like(
+    client, create_and_login_user, create_youtube_video, db_session
+):
+    """
+    Test adding a youtube video like for a video that already is likeed.
+    The endpoint should return a 200 status with no change to the original
+    like state.
+    """
+
+    user: User = await create_and_login_user()
+    video: YoutubeVideo = await create_youtube_video()
+    like = YoutubeVideoLike(email=user.email, video_id=video.id)
+    db_session.add(like)
+    await db_session.commit()
+    original_like_time = like.created_at
+
+    response = await client.put(URL.format(video_id=video.id))
+    assert response.status_code == status.HTTP_200_OK
+
+    db_session.refresh(like)
+    assert original_like_time == like.created_at
