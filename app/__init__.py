1492fda76a9678e5dc0bb142043d3bec8b839065
MohamedRaffik <raffikmohamed7@gmail.com>
2025-08-04 00:00:32 -0400

    feat(admin): add admin router to app

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter, FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
 
+from app.routes.admin import router as admin_router
 from app.routes.authentication import router as auth_router
 from app.routes.music import router as music_router
 from app.routes.youtube import router as youtube_router
@@ -10,6 +11,7 @@ api_router = APIRouter(prefix="/api")
 api_router.include_router(auth_router)
 api_router.include_router(music_router)
 api_router.include_router(youtube_router)
+api_router.include_router(admin_router)
 
 
 app = FastAPI(title="dripdrop", docs_url="/api/docs", openapi_url="/api/openapi.json")
